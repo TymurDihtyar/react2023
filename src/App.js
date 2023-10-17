39b9250df172291ecac3eecf6bcbd370bef52115
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-10-17 12:48:47 +0300

    Hw1_2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import {Persons} from "./components/Persons/Persons";
 
-
 function App() {
   return (
     <div className="App">
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './Person.css';
+import {Button} from "../Button/Button";
 
 const Person = ({value}) => {
     const {id,name,status,species,gender,image} = value
@@ -10,6 +11,7 @@ const Person = ({value}) => {
             <h2>ID:{id} {name}</h2>
             <img src={image} alt={name}/>
             <p>Status: {status} Gender: {gender} Species: {species}</p>
+            <Button props={species}/>
         </div>
     );
 };
