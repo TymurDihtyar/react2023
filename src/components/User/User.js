fdff7d113f26e5ecf11fc7c3cc95258b7186ab56
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-10-17 11:46:40 +0300

    Hw1_1

---
+++ src/components/User/User.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './User.css'
+
+const User  = ({value}) => {
+    const {name, surname, age, info, photo} = value
+    console.log(value);
+    return (
+        <div className = 'member'>
+            <h2>{name} {surname} age:{age}</h2>
+            <img src={photo} alt={name}/>
+            <p>{info}</p>
+        </div>
+    );
+};
+
+export {User};
