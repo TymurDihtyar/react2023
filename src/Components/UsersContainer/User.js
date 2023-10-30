46cb36b649aa1b2fca9fc052213b32dce1d046ad
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-10-30 23:55:31 +0200

    Hw5

--- src/Components/UsersContainer/User.js
+++ src/Components/UsersContainer/User.js
@@ -1,12 +1,9 @@
 const User = ({item}) => {
-    const {id, name, username, email, phone} = item
+    const {id, name} = item
     return (
         <div>
             <div>id: {id}</div>
             <div>name: {name}</div>
-            <div>username: {username}</div>
-            <div>email: {email}</div>
-            <div>phone: {phone}</div>
             <button>UserDetails</button>
         </div>
     );
