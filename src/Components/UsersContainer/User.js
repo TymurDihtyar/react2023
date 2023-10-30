50fcd46ad975e9caeddf3ac61c4acd3f3c63f8c9
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-10-31 00:10:27 +0200

    Hw5

--- src/Components/UsersContainer/User.js
+++ src/Components/UsersContainer/User.js
@@ -1,13 +1,11 @@
 import css from './User.module.css'
+
 const User = ({item}) => {
     const {id, name} = item
     return (
-        <div className={css.render}>
-            <h1>USERS FROM JSONPLASEHOLDER</h1>
-            <div className={css.userBlock}>
-                <div className={css.userData}>#{id} - {name.toUpperCase()}</div>
-                <button className={css.userBut}>UserDetails</button>
-            </div>
+        <div className={css.userBlock}>
+            <div className={css.userData}>#{id} - {name.toUpperCase()}</div>
+            <button className={css.userBut}>Details</button>
         </div>
     );
 };
--- src/Components/UsersContainer/Users.js
+++ src/Components/UsersContainer/Users.js
@@ -1,11 +1,15 @@
 import {User} from "./User";
+import css from "./User.module.css";
 
 const Users = ({users}) => {
 
     return (
-        <div>
-            {users.map(item=> <User key={item.id} item={item}/>)}
-        </div>
+        <>
+            <h1>USERS FROM JSONPLASEHOLDER</h1>
+            <div className={css.render}>
+                {users.map(item=> <User key={item.id} item={item}/>)}
+            </div>
+        </>
     );
 };
 
