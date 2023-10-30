23192cfb5f80785fa78c34e428848c1c1f2817a1
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-10-31 00:05:35 +0200

    Hw5

--- src/Components/UsersContainer/User.js
+++ src/Components/UsersContainer/User.js
@@ -2,10 +2,12 @@ import css from './User.module.css'
 const User = ({item}) => {
     const {id, name} = item
     return (
-        <div className={css.User}>
-            <div>id: {id}</div>
-            <div>name: {name}</div>
-            <button>UserDetails</button>
+        <div className={css.render}>
+            <h1>USERS FROM JSONPLASEHOLDER</h1>
+            <div className={css.userBlock}>
+                <div className={css.userData}>#{id} - {name.toUpperCase()}</div>
+                <button className={css.userBut}>UserDetails</button>
+            </div>
         </div>
     );
 };
