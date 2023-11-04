6f434000d2087a9ff606c0d18e2825e8cd34dc1f
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-04 13:28:02 +0200

    Hw6_2

--- src/Components/CarsContainer/Car.js
+++ src/Components/CarsContainer/Car.js
@@ -9,7 +9,7 @@ const Car = ({car, refresh}) => {
             <div>brand: {brand}</div>
             <div>price: {price}</div>
             <div>year: {year}</div>
-            <Rating name="customized-10" defaultValue={data} precision={0.1} max={10} />
+            <Rating name="customized-10" defaultValue={data} precision={0.1} max={30} />
             <hr/>
         </div>
     );
