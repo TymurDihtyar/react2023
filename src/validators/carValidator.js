742002a95fdf977123d91479dfa73f84f21ab9ff
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-15 11:31:38 +0200

    Hw7.1

--- src/validators/carValidator.js
+++ src/validators/carValidator.js
@@ -1,3 +1,11 @@
 import Joi from "joi";
 
-Joi.object()
+const carValidator = Joi.object({
+    brand: Joi.string().pattern(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required().messages(
+        {
+            'string.pattern.base': 'min 1 to 20 letters'
+        }
+    ),
+    price: Joi.number().min(0).max(1_000_000).required(),
+    year: Joi.number().min(1990).max(new Date().getFullYear()).required()
+})
