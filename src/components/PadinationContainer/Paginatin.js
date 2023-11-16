5095bff6da01fbe4239b52961bf51a9618a834f3
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-16 12:36:35 +0200

    Hw8

--- src/components/PadinationContainer/Paginatin.js
+++ src/components/PadinationContainer/Paginatin.js
@@ -1,20 +1,26 @@
-const Paginatin = () => {
+import css from './Pagination.module.css'
+import {useSelector} from "react-redux";
+
+const Paginatin = ({setQuery}) => {
+    const {prevPage, nextPage} = useSelector(state => state.episodes)
 
     const prev = () => {
         setQuery(prev => {
             prev.set('page', `${+prev.get('page') - 1}`)
+            return prev
         })
     }
     const next = () => {
         setQuery(prev => {
             prev.set('page', `${+prev.get('page') + 1}`)
+            return prev
         })
     }
 
     return (
         <div className={css.buts}>
-            <button disabled={!prevNext.prev} onClick={prev}>prev</button>
-            <button disabled={!prevNext.next} onClick={next}>next</button>
+            <button disabled={!prevPage} onClick={prev}>prev</button>
+            <button disabled={!nextPage} onClick={next}>next</button>
         </div>
     );
 };
--- src/pages/EpisodePage.js
+++ src/pages/EpisodePage.js
@@ -3,11 +3,13 @@ import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {episodeActions} from "../redux/slices/episodeSlice";
 import {useSearchParams} from "react-router-dom";
+import {Paginatin} from "../components/PadinationContainer/Paginatin";
 
 const EpisodePage = () => {
     const [query, setQuery] = useSearchParams({page: '1'});
     const page = query.get('page')
     const dispatch = useDispatch();
+
 
     useEffect(() => {
         setQuery({page: '1'})
@@ -20,6 +22,7 @@ const EpisodePage = () => {
     return (
         <div>
             <Episodes/>
+            <Paginatin setQuery={setQuery}/>
         </div>
     );
 };
