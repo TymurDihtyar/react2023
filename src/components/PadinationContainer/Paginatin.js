7c6d5d32776b6980e10d1ac3c238fa940e0902c0
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-16 12:10:04 +0200

    Hw8

--- src/components/PadinationContainer/Paginatin.js
+++ src/components/PadinationContainer/Paginatin.js
@@ -1,7 +1,20 @@
 const Paginatin = () => {
+
+    const prev = () => {
+        setQuery(prev => {
+            prev.set('page', `${+prev.get('page') - 1}`)
+        })
+    }
+    const next = () => {
+        setQuery(prev => {
+            prev.set('page', `${+prev.get('page') + 1}`)
+        })
+    }
+
     return (
-        <div>
-            
+        <div className={css.buts}>
+            <button disabled={!prevNext.prev} onClick={prev}>prev</button>
+            <button disabled={!prevNext.next} onClick={next}>next</button>
         </div>
     );
 };
