37d2ac6d150fb2875500a018d8759949bcd43f37
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-16 11:43:08 +0200

    Hw8

--- src/pages/EpisodePage.js
+++ src/pages/EpisodePage.js
@@ -2,13 +2,18 @@ import {Episodes} from "../components/EpisodesContainer";
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {episodeActions} from "../redux/slices/episodeSlice";
+import {useSearchParams} from "react-router-dom";
 
 const EpisodePage = () => {
+    const {qery, setQery} = useSearchParams('1')
+    const pages = qery.get('page')
     const {episodes, prevPage, nextPage} = useSelector(state => state.episodes)
     const dispatch = useDispatch();
 
+
+
     useEffect(() => {
-        dispatch(episodeActions.getAllEpisodes('3'))
+        dispatch(episodeActions.getAllEpisodes({page:pages}))
     }, [dispatch]);
 
     console.log(episodes);
